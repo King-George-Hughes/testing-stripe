cc4f5a217a93162ca6107792040fe9cb5a036c5b
unknown <hughesgeorgeking@gmail.com>
2023-06-17 19:09:51 +0000

    changes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { checkout } from "@/checkout";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,10 +14,31 @@ export default function Home() {
         <div className="w-1/2 shadow-md rounded-md m-5 max-w-[500px] p-5">
           <Image src="/images/img1.png" width={500} height={500} alt="img1" />
           <p>Very Expensive Art work 1</p>
+          <button
+            className="px-5 bg-green-600 text-white rounded-sm mt-3"
+            onClick={() =>
+              checkout({
+                lineItems: [
+                  {
+                    price: "price_1NK3utKS72YCaCwXxFIZpkBR",
+                    quantity: 1,
+                  },
+                ],
+              })
+            }
+          >
+            Buy
+          </button>
         </div>
         <div className="w-1/2 shadow-md rounded-md m-5 max-w-[500px] p-5">
           <Image src="/images/img2.png" width={500} height={500} alt="img2" />
           <p>Very Expensive Art work 2</p>
+          <button
+            className="px-5 bg-green-600 text-white rounded-sm mt-3"
+            onClick={() => console.log("Bought")}
+          >
+            Buy
+          </button>
         </div>
       </div>
     </main>
